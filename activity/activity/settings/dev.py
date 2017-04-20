429d80a295948b3d08750c316c708eca005ff3d9
YE <xianyong.ye@monstar-lab-cn.com>
2017-04-20 15:56:20 +0800

    配置setting&修改数据库

---
+++ activity/activity/settings/dev.py
@@ -0,0 +1,25 @@
+from base import *
+
+
+DEBUG=True
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'hacker_earth_db',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'',
+        'PORT':3306,
+        'OPTIONS':{
+
+        }
+    }
+}
+
+
---
+++ activity/activity/settings/prod.py
@@ -0,0 +1,24 @@
+from base import *
+
+
+DEBUG=True
+
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'hacker_earth_db',
+        'USER':'xianyong',
+        'PASSWORD':'123456',
+        'HOST':'',
+        'PORT':3306,
+        'OPTIONS':{
+
+        }
+    }
+}
