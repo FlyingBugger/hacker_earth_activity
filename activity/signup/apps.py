464fac9aaf6728997e6e40089618bae2db31f98f
YE <xianyong.ye@monstar-lab-cn.com>
2017-04-21 10:08:28 +0800

    修改dev setting

--- activity/signup/apps.py
+++ activity/signup/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class SingupConfig(AppConfig):
-    name = 'singup'
+class SignupConfig(AppConfig):
+    name = 'signup'
