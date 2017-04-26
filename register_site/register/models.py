97e423406778282e66ba23d0ee6adcff02d946be
EthanDivestar <divestar@divestardeMacBook-Pro-3.local>
2017-04-26 10:58:22 +0800

    modifying model

--- register_site/register/models.py
+++ register_site/register/models.py
@@ -8,11 +8,28 @@ from rest_framework import serializers
 
 app_label = 'register'
 
+CAMPUS_CHOICE = [
+	('s', '沙河校区'),
+	('q', '清水河校区')
+]
+GRADES = [
+	('f', '大一'),
+	('sf', '大二'),
+	('j', '大三'),
+	('sn', '大四'),
+	('m', '研究生'),
+]
+
 @python_2_unicode_compatible
 class RegisterMessage(models.Model):
-	name=models.CharField(max_length=30,null=False)
-	phone=models.CharField(primary_key=True, max_length=11,null=False)
-	email=models.EmailField(null=False)
+	name = models.CharField(max_length=30,null=False)
+	phone = models.CharField(primary_key=True, max_length=11,null=False)
+	email = models.EmailField(null=False)
+	qq = models.CharField(max_length=12, null=True, blank=True)
+	language = models.CharField(max_length=12, blank=True, null=True)
+	campus = models.CharField(max_length=1, choice=CAMPUS_CHOICE)
+	grade = models.CharField('年级', max_length=2, choice=GRADES)
+	major = models.CharField(max_length=20)
 	register_date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
