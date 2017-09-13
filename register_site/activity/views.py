88f03e9ae9db089c6f63475605053f254a3aca5c
叶同学 <yeli.studio@qq.com>
2017-09-13 10:50:27 +0800

    整理python代码

--- register_site/activity/views.py
+++ register_site/activity/views.py
@@ -12,37 +12,42 @@ import hashlib
 from register.models import *
 from sign import *
 from django.http import JsonResponse
-temp_ticket=""
+
+
+APPID=os.getenv("MONSTAR_WECHAT_PUBLIC_APPID")
+APPSECRET=os.getenv("MONSTAR_WECHAT_PUBLIC_APPSECRET")
+temp_token_url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
+temp_ticket_url="https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token={}"
+JSAPI_ticket=""
+
+
+
 
 
 @require_POST
 def get_wexin_params(request):
 	share_url=request.POST.get("share_url")
-	WEXIN_PARAMS = GetWexinParams (temp_ticket, share_url)
+	WEXIN_PARAMS = GetWexinParams (JSAPI_ticket, share_url)
 	return JsonResponse(WEXIN_PARAMS)
 
 
-def GetWexinParams(p_ticket,full_url):
+def GetWexinParams(ticket,share_url):
 
 	if 0:
-		temp_sign = Sign (p_ticket,full_url).sign ()
-		print "++++"
+		temp_sign = Sign (ticket,share_url).sign ()
 	else:
-		print
-		url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}".format("wxb6d7b7803694fc83","8e7bd1d9b71bc4f1fb7330aa4bd9b992")
-		temp=requests.get(url)
-		json=temp.json()
-		access_token=json["access_token"]
-		expires_in=json["expires_in"]
-		url="https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token={}".format(access_token)
-		temp=requests.get(url)
-		json=temp.json()
-		ticket=json["ticket"]
+		token_url=temp_token_url.format(APPID,APPSECRET)
+		token_json=requests.get(token_url).json()
+		access_token=token_json["access_token"]
+
+		ticket_url=temp_ticket_url.format(access_token)
+		ticket_json=requests.get(ticket_url).json()
+		ticket=ticket_json["ticket"]
+		expires_in=ticket_json["expires_in"]
 		global temp_ticket
 		temp_ticket=ticket
-		temp_sign=Sign(ticket,full_url).sign()
-		print "-------{}".format(temp_sign)
-	return temp_sign
+		sign_json=Sign(ticket,share_url).sign()
+	return sign_json
 
 
 def getMP_varify(request):
